38de2ee4005d52ad77aa120feb1aa98d180131e6
Sagnik Pramanik <sagnikpramanik95@gmail.com>
2023-08-17 06:42:06 +0530

    tts attempt

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import queries;
+import queries
 import speak
+import pyttsx3
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
 print(queries.getGPTAnswer("define avalanche breakdown","explain"))
+engine.runAndWait()
--- queries.py
+++ queries.py
@@ -1,5 +1,13 @@
 import os
 import openai
+import time
+import pyttsx3
+engine = pyttsx3.init()
+engine.setProperty('rate', 150)
+engine.setProperty('pitch', 0.8)  # Sets the pitch to 0.8
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.say("Hello")
 file_path = "S:/key.txt"
 if os.path.isfile(file_path):
     text_file = open(file_path, "r")
@@ -14,5 +22,32 @@ def getGPTAnswer(query,type):
     elif(type == "short"):
         query = " in short, explain " + query
     print(query)
-    chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}])
+    chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": query}],stream = True)
     return chat.choices[0].message.content
+
+delay_time = 0.5 #  faster
+max_response_length = 4000
+answer = ''
+# ASK QUESTION
+prompt = input("Ask a question: ")
+start_time = time.time()
+
+response = openai.ChatCompletion.create(
+    model='gpt-3.5-turbo',
+    messages=[
+        {'role': 'user', 'content': f'{prompt}'}
+    ],
+    max_tokens=max_response_length,
+    temperature=0,
+    stream=True,  # this time, we set stream=True
+)
+
+for event in response:
+    # STREAM THE ANSWER
+    engine.say(answer) # Print the response
+    # RETRIEVE THE TEXT FROM THE RESPONSE
+    event_time = time.time() - start_time  # CALCULATE TIME DELAY BY THE EVENT
+    event_text = event['choices'][0]['delta'] # EVENT DELTA RESPONSE
+    answer = event_text.get('content', '') # RETRIEVE CONTENT
+    time.sleep(delay_time)
+    engine.runAndWait()
