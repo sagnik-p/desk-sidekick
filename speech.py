c0717747dbb2b845f84137e708d10d44eece80be
Sagnik Pramanik <sagnikpramanik95@gmail.com>
2023-08-18 22:25:31 +0530

    added auto switch for offline and online voide audio

--- speech.py
+++ speech.py
@@ -1,24 +1,34 @@
 import pyttsx3
 from gtts import gTTS
+import socket
 from playsound import playsound
-# This module is imported so that we can
-# play the converted audio
 import os
-
-# The text that you want to convert to audio
-
-
-# Language in which you want to convert
 language = 'en'
-myobj = gTTS(text="sure sir, here is what I found", lang=language, slow=False)
-myobj.save("reply.mp3")
-playsound("reply.mp3")
-os.remove("reply.mp3")
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('volume',1.0)
 engine.setProperty('voice', voices[1].id)
 engine.setProperty('rate', 190)
-def say(txt):
+def sayOfffline(txt):
     engine.say(txt)
     engine.runAndWait()
+
+def sayOnline(txt):
+    myobj = gTTS(text=txt, lang=language, slow=False)
+    myobj.save("reply.mp3")
+    playsound("reply.mp3")
+    os.remove("reply.mp3")
+
+def say(txt):
+    if(isOnline()):
+        sayOnline(txt)
+    else
+        sayOfffline(txt)
+
+def isOnline():
+    try:
+        socket.create_connection(("8.8.8.8", 53))
+        return True
+    except OSError:
+        pass
+    return False
